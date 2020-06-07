ccc142bbe2c527b0458df76a47ee9649260692af
Alex <Aditya Pan>
2020-06-07 17:30:48 +0800

    added arrow key input and prevention of illegal movements

--- snake.js
+++ snake.js
@@ -1,15 +1,22 @@
-export const SNAKE_SPEED_PER_SECOND = 2;
+import { getInputDirection } from "./input.js";
 
-const snakeBody = [{ x: 10, y: 11 }, { x: 11, y: 11 }, { x: 12, y: 11 }];
+export const SNAKE_SPEED_PER_SECOND = 1;
+
+const snakeBody = [{ x: 11, y: 11 }];
 
 export function update() {
   let currentHeadSegment = snakeBody.pop();
   let newHeadSegment = { x: 0, y: 0 };
-  newHeadSegment.x = currentHeadSegment.x + 1;
-  newHeadSegment.y = currentHeadSegment.y + 0;
-  snakeBody.shift();
+
+  let moveDirection = getInputDirection();
+
+  newHeadSegment.x = currentHeadSegment.x + moveDirection.x;
+  newHeadSegment.y = currentHeadSegment.y + moveDirection.y;
+
   snakeBody.push(currentHeadSegment);
   snakeBody.push(newHeadSegment);
+
+  snakeBody.shift();
 }
 
 export function render(gameBoard) {
